83ee35b176917c7da857afb220ad88eaaaa27e80
Parth Vaghani <parthvaghani14@gmail.com>
2020-08-05 01:41:13 +0530

    Assignment 2

---
+++ src/components/LeaderComponent.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function RenderLeader(props) {
+    return (
+        <div className="container">
+
+        <div className="row py-3">
+            <div className="col-12 col-sm-2 order-sm-first">
+            <img src={props.leader.image} alt="alberto" className=" img-thumbnail d-flex align-self-center" />
+            </div>
+
+            <div className="col col-sm">
+                <div className="media">
+                    <div className="media-body">
+                        <h4 className="mb-3">{props.leader.name}</h4>
+                        <h6 className="mb-3">{props.leader.designation}</h6>
+                        <p>{props.leader.description}</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        </div>
+    );
+}
+
+
+export default RenderLeader;
