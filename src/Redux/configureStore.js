a09b22e693a4b857a78fdff9f846b1d1e66659db
Parth Vaghani <parthvaghani14@gmail.com>
2020-08-11 12:25:17 +0530

    Combining Reducers

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,7 +1,15 @@
-import {Reducer,initialState} from './reducer';
-import {createStore} from 'redux';
+import {Dishes} from './dishes';
+import {Comments} from './comments';
+import {Leaders} from './leaders';
+import {Promotions} from './promotions';
+import { createStore, combineReducers } from 'redux';
 
 export const configureStore = () => {
-        const store  = createStore(Reducer,initialState);
-        return store;
+    const store = createStore(combineReducers({
+        dishes: Dishes,
+        leaders: Leaders,
+        promotions: Promotions,
+        comments: Comments
+    }));
+    return store;
 }
